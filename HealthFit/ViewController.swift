aff8152da4cdbdd978270a8fe4717b1b1fb36c59
Eanguy Eng <eeng1@mail.sfsu.edu>
2021-12-06 16:33:08 -0800

    Initial Commit

---
+++ HealthFit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthFit
+//
+//  Created by Ryan Eng on 12/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
