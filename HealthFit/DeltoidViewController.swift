2707a8e7a08ed34939979b0a24361c4a37acfb4a
Eanguy Eng <eeng1@mail.sfsu.edu>
2021-12-09 17:15:08 -0800

    Add new Screen

---
+++ HealthFit/DeltoidViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DeltoidViewController.swift
+//  HealthFit
+//
+//  Created by Ryan Eng on 12/9/21.
+//
+
+import UIKit
+
+class DeltoidViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Deltoid Workout"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
